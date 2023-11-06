52745b4744d3cedfd4b42abc41313f82156b721c
Mykola32 <shumyk26@gmail.com>
2023-11-06 20:11:28 +0200

    final2

---
+++ pythonProject/projMax/tabel/app1/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+class LogisticInfo(models.Model):
+    distance = models.FloatField(default=0, blank=False)
+    type_delivery = models.CharField(max_length=10, blank=False)
+    masa = models.FloatField(default=0, blank=False)
+    pib = models.CharField(max_length=50, blank=False)
+    phone = models.CharField(max_length=13, blank=False)
+    email = models.EmailField(max_length=30, blank=False)
+
+
+class TextFile(models.Model):
+    file = models.FileField('Текстовий файл', null=True)
+    text_date = models.DateTimeField("Дата створення файлу")
